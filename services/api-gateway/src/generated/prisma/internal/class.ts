d574a4fb78065c85c3caa7b7d5e46d06fc7ee48b
Matteus Varlesse <varlesse04@gmail.com>
2026-04-15 22:30:40 -0300

    fix: errors in containers

--- services/api-gateway/src/generated/prisma/internal/class.ts
+++ services/api-gateway/src/generated/prisma/internal/class.ts
@@ -23,7 +23,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "/app/src/generated/prisma",
+      "value": "/workspace/services/api-gateway/src/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -37,7 +37,7 @@ const config: runtime.GetPrismaClientConfig = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/app/prisma/schema.prisma",
+    "sourceFilePath": "/workspace/services/api-gateway/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../../../prisma",
@@ -47,6 +47,7 @@ const config: runtime.GetPrismaClientConfig = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
